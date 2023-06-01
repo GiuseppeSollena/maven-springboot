7bb503ea10c3bd21b819b618dc10a43c055dea16
Giuseppe Sollena <92548016+GiuseppeSollena@users.noreply.github.com>
2023-06-01 16:39:42 +0200

    Add files via upload

---
+++ src/main/java/it/molianri/App.java
@@ -0,0 +1,13 @@
+package it.molianri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
