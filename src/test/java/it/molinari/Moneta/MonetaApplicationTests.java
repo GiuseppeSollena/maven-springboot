64aebc0fb98a76ef0cd4a067962542809cc5eb3e
Beppe Sollena <sollena.giuseppe@gmail.com>
2023-06-01 16:44:22 +0200

    master: setup project

---
+++ src/test/java/it/molinari/Moneta/MonetaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.molinari.Moneta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
